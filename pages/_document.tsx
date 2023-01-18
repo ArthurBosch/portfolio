3fd4496d999e8a5d96c63d7a83873a0d3ca68e4c
ArthurBosch <mr.bolshakov12@gmail.com>
2023-01-18 20:31:06 +0200

    font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,29 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="preload"
+          href="./font/ResistMono-Rg.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin="anonymous"
+        ></link>
+        <link
+          rel="preload"
+          href="./font/ResistMono-Bd.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin="anonymous"
+        ></link>
+        <link
+          rel="preload"
+          href="./font/ResistMono-Lt.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin="anonymous"
+        ></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
